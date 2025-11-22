151b6ef36c696a0dbe121e169052d6250776585d
Soham <soham.builds@gmail.com>
2025-11-22 16:43:58 +0530

    resolved cors error

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,6 +13,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true // allow frontend to send cookies
+}))
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
